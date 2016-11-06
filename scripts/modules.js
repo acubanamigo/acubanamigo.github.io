f3053e0c98d71b2afd8929014d6f92bfdc122aec
Robert Stawinski <sta15015@byui.edu>
2016-11-05 20:11:40 -0600

    update

---
+++ scripts/modules.js
@@ -0,0 +1,12 @@
+// A JQuery function to implement modularization
+
+$(function(){
+    $("#page-header").load("../modules/header.html");
+    $("#page-nav").load("../modules/nav.html");
+    $("#footer").load("../modules/footer.html");
+});
+
+    
+    
+    
+    
