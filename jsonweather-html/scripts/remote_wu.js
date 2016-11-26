00f3ebd336df6026aa180c5ce27d3c700d1f07b1
Robert Stawinski <sta15015@byui.edu>
2016-11-26 13:26:42 -0700

    update

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -23,18 +23,29 @@ $(function () {
   // Get the data from the wunderground API
   function getData(lat, long){
     $.ajax({
+    url:"https://api.wunderground.com/api/2e44794c0c8e3b62/geolookup/conditions/q/"+ lat + "," + long + ".json", 
+        dataType: "jsonp",
+        success: function(data){
+                console.log(data);
+                var location = data.location.city + ', ' + data.location.state;
+                var temp = Math.round(data.current_observation.temp_f);
+                var summary = data.current_observation.weather;
+                var precip = data.current_observation.precip_today_in;
+                var feelsLike = data.current_observation.feelslike_f;
+                var windDir = data.current_observation.wind_dir;
+                $('#cityDisplay').text(location);
+                $('title').text(location + ' | Weather Home');
+                $('#currentTemp').text(temp + "°F");
+                $('#summary').text(summary);
+                $('#add1').text("Feels Like: " + feelsLike + "°F");
+                $('#add2').text("Precipitation: " + precip + " inches");
+                $('#add3').text("Wind Direction: " + windDir);
+                $("#cover").fadeOut(250);
+        
+        }
+          });
 
-
-
-
-
-
-
-      $("#cover").fadeOut(250);
-    }
-           });
-
-  }
+                            } 
 
   // A function for changing a string to TitleCase
   function toTitleCase(str){
