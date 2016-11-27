a500a841133cc2b647aaef6f798eacf3b27a1f31
Robert Stawinski <sta15015@byui.edu>
2016-11-26 21:28:04 -0700

    update

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -23,29 +23,35 @@ $(function () {
   // Get the data from the wunderground API
   function getData(lat, long){
     $.ajax({
-    url:"https://api.wunderground.com/api/2e44794c0c8e3b62/geolookup/conditions/q/"+ lat + "," + long + ".json", 
+    url: "https://api.wunderground.com/api/2e44794c0c8e3b62/geolookup/conditions/q/" + lat + "," + long + ".json", 
         dataType: "jsonp",
-        success: function(data){
-                console.log(data);
+        success: function (data)
+        {
+            
                 var location = data.location.city + ', ' + data.location.state;
+            
+                // Rounding the temp to the nearest integer
                 var temp = Math.round(data.current_observation.temp_f);
+            
+                // Summar of the current conditions
                 var summary = data.current_observation.weather;
-                var precip = data.current_observation.precip_today_in;
+                var zipCode = data['location']['zip'];
                 var feelsLike = data.current_observation.feelslike_f;
                 var windDir = data.current_observation.wind_dir;
-                $('#cityDisplay').text(location);
-                $('title').text(location + ' | Weather Home');
-                $('#currentTemp').text(temp + "°F");
-                $('#summary').text(summary);
-                $('#add1').text("Feels Like: " + feelsLike + "°F");
-                $('#add2').text("Precipitation: " + precip + " inches");
-                $('#add3').text("Wind Direction: " + windDir);
+                $("#cityDisplay").text(location);
+                $("title").text(location + " |  Weather Home");
+                $("#currentTemp").text(temp + "°F");
+                $("#summary").text(summary);
+                $("#add1").text("Feels Like: " + feelsLike + "°F");
+                $("#add2").text("Wind Direction: " + windDir);
+                $("#add3").text("Zip Code: " + zipCode);
+             
                 $("#cover").fadeOut(250);
         
         }
           });
 
-                            } 
+         } 
 
   // A function for changing a string to TitleCase
   function toTitleCase(str){
