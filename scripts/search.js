21e4adebefb80c1c34cdfbbdc3447a22b62a22b2
Robert Stawinski <sta15015@byui.edu>
2016-12-02 18:29:43 -0700

    update

--- scripts/search.js
+++ scripts/search.js
@@ -1,68 +1,73 @@
-var return;
+var returned;
 
-
-
-
-
-//right under console.log(data); write v inside getJson which is in .keyUp()
-//returned = data;
-
-
-//onClick function
-//var jsonCity
-//console.log(jsonCIty); V
-
-// in .on()
-
-//index = $(this).index("a");
-//getData(returned.RESULTS[index].lat, returned.RESULTS[index].lon)
-
-
-$('#query').keyup(function() {
+$('#query').keyup(function(){
+  // All code will be inside of this block
   var value = $('#query').val();
   var rExp = new RegExp(value, "i");
-  $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
-    console.log(data); // test for JSON received
+  $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+    console.log(data);
+    returned = data;
+
     // Begin building output
-    returned = data;  
-    
     var output = '<ol>';
     $.each(data.RESULTS, function(key, val) {
       if (val.name.search(rExp) != -1) {
         output += '<li>';
-        output += '<a href="https://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+        output += '<a href="http://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
         output += '</li>';
       }
     }); // end each
     output += '</ol>';
     $("#searchResults").html(output); // send results to the page
   }); // end getJSON
-}); // end onkeyup
 
-// Get weather data from wunderground.com
-function getData(input) {
-  // Get the data from the wunderground API
+
+
+  // Intercept the menu link clicks
+  $("#searchResults").on("click", "a", function(evt) {
+    evt.preventDefault();
+
+    // With the text value get the needed value from the weather.json file
+    var jsonCity = $(this).text(); // Franklin, etc...
+    console.log(jsonCity);
+    index = $(this).index("a");
+    console.log(index);
+    console.log(returned);
+    getData(returned.RESULTS[index].lat, returned.RESULTS[index].lon);
+
+}); // end keyup
+});
+
+function getData(lat, long) {
   $.ajax({
-    url: "http://api.wunderground.com/api/2e44794c0c8e3b62/geolookup/conditions/q/"
-    + input + ".json"
-    , dataType: "jsonp"
-    , success: function (data) {
+    url : 'https://api.wunderground.com/api/2e44794c0c8e3b62/geolookup/conditions/forecast/q/' + lat + ',' + long + '.json',
+    dataType : "jsonp",
+    success : function(data){
       console.log(data);
       var location = data.location.city + ', ' + data.location.state;
-      var temp_f = data.current_observation.temp_f;
-      console.log('Location is: ' + location);
-      console.log('Temp is: ' + temp_f);
-      $("#cityDisplay").text(location);
-      $("title").html(location + " | Weather Center");
-      $("#currentTemp").html(Math.round(temp_f) + '°');
-      $("#summary").text(toTitleCase(data.current_observation.icon));
-      $("#cover").fadeOut(250);
+      var temp = Math.round(data.current_observation.temp_f);
+      var high = Math.round(data.forecast.simpleforecast.forecastday[0].high.fahrenheit);
+      var low = Math.round(data.forecast.simpleforecast.forecastday[0].low.fahrenheit);
+      var summary = data.current_observation.weather;
+      var feelsLike = data.current_observation.feelslike_f;
+      var windspeed = data.current_observation.wind_mph;
+      var visibility = data.current_observation.visibility_mi;
+      $('#h1').text(location);
+      $('title').text(location + ' | Weather Home');
+      $('#currenttemp').text(temp + "°F");
+      $('#high').text(high + "°F");
+      $('#low').text(low + "°F");
+      $('#summary').text(summary);
+      $('#feelslike').text("Feels Like: " + feelsLike + "°F");
+      $('#windspeed').text("Windspeed: " + windspeed + " mph");
+      $('#visibility').text("Visibility: " + visibility + " mi");
     }
   });
 }
 
+// A function for changing a string to TitleCase
+function toTitleCase(str) {
+  return str.replace(/\w+/g, function (txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                                            });
+};
 
- // A function for changing a string to TitleCase
-  function toTitleCase(str){
-    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
