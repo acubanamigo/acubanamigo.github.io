eb16a8cb815a735ad4eb615c3a5de86147b9a9a4
Robert Stawinski <sta15015@byui.edu>
2016-12-08 14:01:07 -0700

    brackets

---
+++ final-project/scripts/modules.js
@@ -0,0 +1,7 @@
+// Include modules into the html file
+// Pathes may need altered depending on site structure
+$(function(){
+  $("#page-header").load("/final-project/modules/footer.html");
+  $("#page-nav").load("/final-project/modules/nav.html");
+  $("#footer-content").load("../final-project/modules/footer.html");
+});
--- scripts/search.js
+++ scripts/search.js
@@ -68,6 +68,8 @@ function getData(lat, long) {
   });
 }
 
+  
+  
 // A function for changing a string to TitleCase
 function toTitleCase(str) {
   return str.replace(/\w+/g, function (txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
