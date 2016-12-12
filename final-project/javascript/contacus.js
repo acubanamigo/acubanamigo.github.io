ce56d45b433a1c026dffc38e9fb7a0589f466df7
Robert Stawinski <sta15015@byui.edu>
2016-12-12 14:42:06 -0700

    update

---
+++ final-project/javascript/contacus.js
@@ -0,0 +1,26 @@
+function getData(){
+$.ajax({
+        url: "/javascript/contactus.json",
+        success: function(data){ useData(data);}
+
+    });
+}
+
+function useData(data){
+    
+    console.log(data);
+    $("#city").html(data)['city']);
+    $("#state").html(data)['state']);
+    $("#email").html(data)['email']);
+    
+}
+    
+                
+            
+        }
+
+  // A function for changing a string to TitleCase
+  function toTitleCase(str){
+    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+});
