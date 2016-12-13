a5064967c1aac348b5b00ce6a5005c66c234c06d
Robert Stawinski <sta15015@byui.edu>
2016-12-13 11:01:21 -0700

    update

--- final-project/javascript/contacus.js
+++ final-project/javascript/contacus.js
@@ -1,25 +1,41 @@
-function getData(){
-$.ajax({
-        url: "/javascript/contactus.json",
-        success: function(data){ useData(data);}
+$(function () {
 
+    getData();
+
+
+});
+
+function getData() {
+    $.ajax({
+        url: "/final-project/javascript/contactus.json",
+        success: function (data) {
+            console.log(data);
+            $("#contactus").html(data['Contact Us']);
+
+            $("#name").html(data['Contact Us']['Name']);
+
+            $("#city").html(data['Contact Us']['City']);
+
+            $("#state").html(data['Contact Us']['State']);
+
+            $("#email").html(data['Contact Us']['Email']);
+        }
     });
 }
 
-function useData(data){
-    
+function useData(data) {
+
     console.log(data);
-    $("#city").html(data)['city']);
-    $("#state").html(data)['state']);
-    $("#email").html(data)['email']);
-}
+
+    $("name").html(data['Contact Us']['Name']);
+
+    $("#city").html(data['Contact Us']['City']);
+
+    $("#state").html(data['Contact Us']['State']);
+
+    $("email").html(data['Contact Us']['Email']);
 
 
-                
-            
+};
 
-  // A function for changing a string to TitleCase
-  function toTitleCase(str){
-    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
-});
+    
